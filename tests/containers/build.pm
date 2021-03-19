ab40202d17f82e7ebde8ab1ea511f6076adf8859
Amrysliu <xiaojing.liu@suse.com>
2021-03-19 14:32:44 +0800

    xxx

--- tests/containers/build.pm
+++ tests/containers/build.pm
@@ -5,6 +5,7 @@ use testapi;
 sub run {
     my ($self) = @_;
     assert_script_run("git clone https://github.com/os-autoinst/openQA.git", timeout => 300);
+    assert_script_run('sed -i \'s/\" kvm/\" --non-unique kvm/\' openQA/container/worker/run_openqa_worker.sh', timeout => 30);
     assert_script_run("docker build openQA/container/webui -t openqa_webui", timeout => 600);
     assert_script_run("docker build openQA/container/worker -t openqa_worker", timeout => 600);
     assert_script_run("docker build openQA/container/openqa_data -t openqa_data", timeout => 600);
